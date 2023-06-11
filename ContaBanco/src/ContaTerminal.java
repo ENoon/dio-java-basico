934dba73fc3e36043cdfba25f3d175dc5ac077fb
ENoon <noonsilva@hotmail.com>
2023-06-11 17:17:30 -0300

    prg completo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Por favor, digite o número da Agência: ");
+    String agencia = scan.next();
+
+    System.out.print("Digite o numero da conta: ");
+    int numero = scan.nextInt();
+
+    System.out.print("Digite o seu nome: ");
+    String nomeCliente = scan.next();
+
+    System.out.print("Digite seu saldo: ");
+    Float saldo = scan.nextFloat();
+
+    System.out
+        .println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+            ", conta: " + numero + " e seu saldo " + saldo + " já está disponivel para saque");
+  }
 }
